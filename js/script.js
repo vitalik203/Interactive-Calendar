5be2d3e13bca3417b57bef152317ccebd700329c
Zaluskii Vitalii <vzaluskiy23@gmail.com>
2024-01-09 13:02:47 +0200

    finished calendar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 const now = new Date()
-// console.log(now);
-let month = now.getMonth
-let year = now.getFullYear
-
+let month = now.getMonth()
+let year = now.getFullYear()
 const previous = document.querySelector(".prev")
 const next = document.querySelector(".next")
 const month_year = document.querySelector(".calendar__information")
-
-const elements = [...document.querySelector(".calendar__el")]
-console.log(elements);
+// let data = now.getDate()
+// console.log(data);
+const elements = [...document.querySelectorAll(".calendar__el")]
 const listOfMonth = {
     0:"January",
     1:"February",
@@ -31,6 +29,7 @@ previous.addEventListener("click", function(){
     }else{
         month--
     }
+    render(month, year)
 })
 
 next.addEventListener("click", function(){
@@ -40,12 +39,28 @@ next.addEventListener("click", function(){
     }else{
         month++
     }
+    render(month, year)
 })
 
 
-function render(listOfMonth, year){
-    const firstDay = new Date(listOfMonth+1, year, 0).getDate()
-    console.log(firstDay);
+function render(monthIndex, year){
+    const numDaysInMonth = new Date(year, monthIndex+1, 0).getDate()
+    const firstDate = new Date(year, monthIndex)
+    const firstDay = new Date(firstDate).getDay()
+    month_year.innerText = `${year} \ ${listOfMonth[monthIndex]}`
+    elements.forEach((element, index)=>{
+        const dateNumber = (index+1)-firstDay;
+        if(dateNumber>0&&dateNumber<=numDaysInMonth){
+            element.innerText = dateNumber
+        }else{
+            element.innerText = " "
+        }
+        if(now.getFullYear()===year && now.getMonth()===monthIndex && now.getDate()===dateNumber){
+            element.classList.add("today")
+        }else{
+            element.classList.remove("today")
+        }
+    })
 }
 
 render(month, year);
